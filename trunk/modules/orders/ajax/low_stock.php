d19139eaa8fcb336ec1ee2e9b29e2549e4bc058e
gonetil <gonetil@2ccb1478-6fb6-82df-e304-f737d9f2d76f>
2011-08-14 02:16:39 +0000

    en el query de low_stock, cambio el where donde decia quantity_on_order por reorder_quantity, para  q se consideren solo aquellos productos que tienen cargada la cantidad a comprar

--- trunk/modules/orders/ajax/low_stock.php
+++ trunk/modules/orders/ajax/low_stock.php
@@ -4,7 +4,9 @@
 		$result = $db->Execute("select id, short_name from " . TABLE_CONTACTS . " where type = 'v' and short_name = '".$_GET['vendor']."' " );
 		
 		$json = array();
+		
 		if (!$result->EOF){
+			
 			$vendor_id = $result->fields['id'];
 			/**
 			 * Esta era la version original, ahora la cambio para usar lo mismo que le propusimos a Cesar
@@ -12,7 +14,7 @@
 			 */
 			
 			$result_second = $db->Execute("SELECT id, reorder_quantity as qty FROM ".TABLE_INVENTORY. " 
-										   WHERE quantity_on_hand < minimum_stock_level and inactive = '0'and vendor_id = $vendor_id and quantity_on_order > 0");
+										   WHERE quantity_on_hand < minimum_stock_level and inactive = '0'and vendor_id = $vendor_id and reorder_quantity > 0");
 			$i = 0;
 			while (!$result_second->EOF){
 				$json [$result_second->fields['id'] ]= $result_second->fields['qty'] ;
